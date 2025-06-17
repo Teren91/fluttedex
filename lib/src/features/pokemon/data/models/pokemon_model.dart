5486a608bd23c3f4123ce23c7eb53195011d11a5
Teren91 <Teren_91@hotmail.com>
2025-06-17 23:31:31 +0200

    fix(pokemon): add pokemon id

--- lib/src/features/pokemon/data/models/pokemon_model.dart
+++ lib/src/features/pokemon/data/models/pokemon_model.dart
@@ -1,10 +1,15 @@
 import 'package:fluttedex/src/features/pokemon/domain/entities/pokemon.dart';
 
 class PokemonModel extends Pokemon {
-  const PokemonModel({required super.name, required super.url});
+  const PokemonModel({
+    required super.id,
+    required super.name,
+    required super.url,
+  });
 
   factory PokemonModel.fromJson(Map<String, dynamic> json) {
     return PokemonModel(
+      id: json['id'] as int,
       name: json['name'] as String,
       url: json['url'] as String,
     );
@@ -12,6 +17,7 @@ class PokemonModel extends Pokemon {
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'name': name,
       'url': url,
     };
--- lib/src/features/pokemon/domain/entities/pokemon.dart
+++ lib/src/features/pokemon/domain/entities/pokemon.dart
@@ -1,11 +1,12 @@
 import 'package:equatable/equatable.dart';
 
 class Pokemon extends Equatable {
+  final int id;
   final String name;
   final String url;
 
-  const Pokemon({required this.name, required this.url});
+  const Pokemon({required this.id, required this.name, required this.url});
 
   @override
-  List<Object?> get props => [name, url];
+  List<Object?> get props => [id, name, url];
 }
