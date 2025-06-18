16d347ca8ffa4664716cc2b455a4878fa6db9ce8
Teren91 <Teren_91@hotmail.com>
2025-06-18 20:09:16 +0200

    feat(pokemon detail): define data layer repository

--- lib/src/features/pokemon/data/repositories/pokemon_repository_impl.dart
+++ lib/src/features/pokemon/data/repositories/pokemon_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:fluttedex/src/features/pokemon/data/datasources/pokemon_remote_data_source.dart';
 import 'package:fluttedex/src/features/pokemon/domain/entities/pokemon.dart';
+import 'package:fluttedex/src/features/pokemon/domain/entities/pokemon_detail.dart';
 import 'package:fluttedex/src/features/pokemon/domain/repositories/pokemon_repository.dart';
 
 class PokemonRepositoryImpl implements PokemonRepository {
@@ -22,6 +23,15 @@ class PokemonRepositoryImpl implements PokemonRepository {
       return await remoteDataSource.getPokemonByName(name);
     } catch (e) {
       throw Exception('Failed to load pokemon');
+    }
+  }
+
+  @override
+  Future<PokemonDetail> getPokemonDetail(String nameOrId) async {
+    try {
+      return await remoteDataSource.getPokemonDetail(nameOrId);
+    } catch (e) {
+      throw Exception('Failed to load pokemon details');
     }
   }
 }
